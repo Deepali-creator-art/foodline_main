904630c1a9d22af64dddabdbf092ab58e602669d
Deepali-creator-art <134923246+Deepali-creator-art@users.noreply.github.com>
2024-09-22 17:51:03 +0530

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,HttpResponse,redirect
+from .forms import UserForm
+from .models import User
+def registerUser(request):
+    if(request.method=='POST'):
+        form=UserForm(request.POST)
+        if form.is_valid():
+            password=form.cleaned_data['password']
+            user=form.save(commit=False)
+            user.set_password(password)
+            user.role=User.CUSTOMER
+            user.save()
+            return redirect('registerUser')
+    else:
+        form=UserForm()
+    context={
+        'form':form
+    }
+    return render(request,'accounts/registerUser.html',context)
