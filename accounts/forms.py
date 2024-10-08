53494c60b882c41e63854e666af8b27a42b83456
Deepali-creator-art <134923246+Deepali-creator-art@users.noreply.github.com>
2024-10-08 15:10:16 +0530

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms 
-from .models import User 
+from .models import User,UserProfile
+from .validation import allow_only_images_validator
 class UserForm(forms.ModelForm):
     password=forms.CharField(widget=forms.PasswordInput())
     confirm_password=forms.CharField(widget=forms.PasswordInput())
@@ -13,4 +14,14 @@ class UserForm(forms.ModelForm):
         confirm_password=cleaned_data.get('confirm_password')
         if(password!=confirm_password):
             raise forms.ValidationError("Password does not match")
+class UserProfileForm(forms.ModelForm):
+    profile_picture=forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}),validators=[allow_only_images_validator])
+    cover_photos=forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}),validators=[allow_only_images_validator])
+    address=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Start typing....','required':'required'}))
+    latitude=forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    longitude=forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))  
+    class Meta:
+        model=UserProfile()
+        fields=['profile_picture','cover_photos','address','country','state','city','pin_code','latitude',' longitude']
+            
             
