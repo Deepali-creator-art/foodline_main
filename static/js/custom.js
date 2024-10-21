83652d077c7a724b7a6856ae9c48fdea68f7d0eb
Deepali-creator-art <134923246+Deepali-creator-art@users.noreply.github.com>
2024-10-21 16:19:25 +0530

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,59 +1,38 @@
-let autocomplete;
-
-function initAutoComplete(){
-autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('id_address'),
-    {
-        types: ['geocode', 'establishment'],
-        //default in this app is "IN" - add your country code
-        componentRestrictions: {'country': ['in']},
-    })
-// function to specify what should happen when the prediction is clicked
-autocomplete.addListener('place_changed', onPlaceChanged);
-}
-
-function onPlaceChanged (){
-    var place = autocomplete.getPlace();
-
-    // User did not select the prediction. Reset the input field or alert()
-    if (!place.geometry){
-        document.getElementById('id_address').placeholder = "Start typing...";
-    }
-    else{
-        console.log('place name=>', place.name)
-    }
-    // get the address components and assign them to the fields
-}
-$(document).ready(funtion(){
+$(document).ready(function(){
     $('.add_to_cart').on('click',function(e){
         e.preventDefault();
         food_id=$(this).attr('data-id');
         url=$(this).attr('data-url');
         data={
-            'food_id':food_id
+            'food_id':food_id,
         }
         $.ajax({
             type:'GET',
             url:url,
             data:data,
             success:function(response){
-                console.log(response)
+                if(response.status=='login_required'){
+                    swal (response.message,'','info').then(function(){
+                        window.location='/accounts/login';
+                    })
+                }else if(response.status=='Failed'){
+                    swal (response.message,'','error')
+                }else{
+                    $('#cart_counter').html(response.cart_counter['cart_count'])
+                    $('#qty-'+food_id).html(response.qty)
+                    //subtotal,tax,total
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax_dict'],
+                        response.cart_amount['grand_total'],
+    
+                        ) 
+                    
+
+                }
+                
             }
         })
-
-    })
-    //place the cart item quantity on load
-    $('.item_qty').each(function(){
-        var the_id=$(this).attr('id')
-        var qty=$(this).attr('data-qty')
-        $('#'+the_id).html(qty)
-    })
-     
-
-});
-
         
-        
-                        
-    
-   
+    })
+})
