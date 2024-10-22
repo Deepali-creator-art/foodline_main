d5d665ececd0dbb30aa8ffc77c6e22ac367507a4
Deepali-creator-art <134923246+Deepali-creator-art@users.noreply.github.com>
2024-10-22 14:46:28 +0530

    Update models.py

--- marketplace/models.py
+++ marketplace/models.py
@@ -11,3 +11,11 @@ class Cart(models.Model):
     updated_at=models.DateTimeField(auto_now=True)
     def __unicode__(self):
         return self.user
+class Tax(models.Model):
+    tax_type=models.CharField(max_length=50,unique=True)
+    tax_percentage=models.DecimalField(decimal_places=2,max_digits=4,verbose_name='TaxPercentage(%)')
+    is_active=models.BooleanField(default=True)
+    class Meta:
+        verbose_name_plural='tax'
+    def __str__(self):
+        return self.tax_type
