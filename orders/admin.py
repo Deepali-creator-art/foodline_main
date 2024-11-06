c90a53dcbbd432cc979225f3d963747e899518fc
Deepali-creator-art <134923246+Deepali-creator-art@users.noreply.github.com>
2024-11-06 11:30:16 +0530

    Add files via upload

--- orders/admin.py
+++ orders/admin.py
@@ -5,7 +5,7 @@ class orderedfoodonline(admin.TabularInline):
     readonly_fields=('order','payment','user','fooditem','quantity','price','amount')
     extra=0
 class OrderAdmin(admin.ModelAdmin):
-    list_display=['order_number','name','phone','email','total','payment_method','status','is_ordered']
+    list_display=['order_number','name','phone','email','total','payment_method','status','order_placed_to','is_ordered']
     inlines=[orderedfoodonline]
 admin.site.register(Payment)
 admin.site.register(Order,OrderAdmin)
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect,HttpResponse
 from django.http import HttpResponse,JsonResponse
-from marketplace.models import Cart
+from marketplace.models import Cart,FoodItem
 from .forms import orderform
 from .models import Order,Payment,OrderedFood
 import simplejson as json
@@ -9,6 +9,7 @@ from marketplace.context_processor import get_cart_amount
 from accounts.utils import send_notification
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
+from marketplace.models import Tax
 import logging
 @login_required(login_url='login')
 def place_order(request):
@@ -16,6 +17,42 @@ def place_order(request):
     cart_count=cart_items.count()
     if cart_count<=0:
         return redirect('marketplace')
+    
+    vendors_ids=[]
+    for i in cart_items:
+        if i.fooditem.vendor.id not in vendors_ids:
+            vendors_ids.append(i.fooditem.vendor.id)
+    #{'vendor_id':{'subtotal':{'tax_type':{'tax_percentage':'tax_amount'}}}}       
+    get_tax=Tax.objects.filter(is_active=True)
+    subtotal=0
+    total_data={}
+    k={}
+    for i in cart_items:
+        fooditem=FoodItem.objects.get(pk=i.fooditem.id,vendor_id__in=vendors_ids)
+        v_id=fooditem.vendor.id
+        if v_id in k:
+            subtotal=k[v_id]
+            subtotal+=(fooditem.price*i.quantity)
+            k[v_id]=subtotal
+        else:
+            subtotal=(fooditem.price*i.quantity)
+            k[v_id]=subtotal
+        print(k)
+        #calculate tax_data
+        tax_dict={}
+        for i in get_tax:
+            tax_type=i.tax_type
+            tax_percentage=i.tax_percentage
+            tax_amount=round((tax_percentage*subtotal)/100,2)
+            tax_dict.update({tax_type:{str(tax_percentage):str(tax_amount)}})
+        #construct total data
+        total_data.update({fooditem.vendor.id:{str(subtotal):str(tax_dict)}})
+        print(total_data)
+        print(tax_dict)
+        
+        print(fooditem,fooditem.vendor.id)
+    
+    
     subtotal=get_cart_amount(request)['subtotal']
     total_tax=get_cart_amount(request)['tax']
     grand_total=get_cart_amount(request)['grand_total']
@@ -36,10 +73,12 @@ def place_order(request):
             order.user=request.user
             order.total=grand_total
             order.tax_data=json.dumps(tax_data)
+            order.total_data=json.dumps(total_data)
             order.total_tax=total_tax
             order.payment_method=request.POST['payment_method']
             order.save() #order id pk is generated
             order.order_number=generate_order_number(order.id)
+            order.vendors.add(*vendors_ids)
             order.save()
             context={
                 'order':order,
@@ -129,7 +168,7 @@ def payments(request):
 def order_complete(request):
     # Initialize logger
     logger = logging.getLogger(__name__)
-    order_number=request.GET.get('order_no')
+    order_number=request.GET.get('order_number')
     transaction_id=request.GET.get('transaction_id')
     try:
         order=Order.objects.get(order_number=order_number,payment__transaction_id=transaction_id,is_ordered=True)
